418086493e5ec81443ebfd3adc2ffaf590b29c28
holy-flower <prodolnova04@mail.ru>
2024-05-20 09:08:00 +0300

    исправлено

--- app/src/main/java/com/example/proekt_12/MyContentProvider.java
+++ app/src/main/java/com/example/proekt_12/MyContentProvider.java
@@ -11,27 +11,32 @@ import android.database.sqlite.SQLiteException;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
-
 import java.util.HashMap;
 
 public class MyContentProvider extends ContentProvider {
     public MyContentProvider() {
     }
+    // Имя провайдера контента
     static final String PROVIDER_NAME = "com.demo.user.provider";
+    // URL-адрес для доступа к данным провайдера контента
     static final String URL = "content://" + PROVIDER_NAME + "/users";
+    // URI-адрес для доступа к данным провайдера контента
     static final Uri CONTENT_URI = Uri.parse(URL);
+    // Названия столбцов таблицы
     static final String id = "id";
     static final String name = "name";
+    // Код для UriMatcher
     static final int uriCode = 1;
     static final UriMatcher uriMatcher;
+    // Хэш-карта для хранения значений
     private static HashMap<String, String> values;
-
+    // Инициализация UriMatcher
     static {
         uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
         uriMatcher.addURI(PROVIDER_NAME, "users", uriCode);
         uriMatcher.addURI(PROVIDER_NAME, "users/*", uriCode);
     }
-
+    // Метод для получения типа MIME
     @Override
     public String getType(Uri uri) {
         switch (uriMatcher.match(uri)) {
@@ -41,7 +46,7 @@ public class MyContentProvider extends ContentProvider {
                 throw new IllegalArgumentException("Unsupported URI: " + uri);
         }
     }
-
+    // Метод для инициализации провайдера контента
     @Override
     public boolean onCreate() {
         Context context = getContext();
@@ -52,7 +57,7 @@ public class MyContentProvider extends ContentProvider {
         }
         return false;
     }
-
+    // Метод для выполнения запроса к провайдеру контента
     @Override
     public Cursor query(Uri uri, String[] projection, String selection,
                         String[] selectionArgs, String sortOrder) {
@@ -73,7 +78,7 @@ public class MyContentProvider extends ContentProvider {
         c.setNotificationUri(getContext().getContentResolver(), uri);
         return c;
     }
-
+    // Метод для вставки данных в провайдер контента
     @Override
     public Uri insert(Uri uri, ContentValues values) {
         long rowID = db.insert(TABLE_NAME, "", values);
@@ -84,7 +89,7 @@ public class MyContentProvider extends ContentProvider {
         }
         throw new SQLiteException("Failed to add a record into " + uri);
     }
-
+    // Метод для обновления данных в провайдере контента
     @Override
     public int update(Uri uri, ContentValues values, String selection,
                       String[] selectionArgs) {
@@ -99,7 +104,7 @@ public class MyContentProvider extends ContentProvider {
         getContext().getContentResolver().notifyChange(uri, null);
         return count;
     }
-
+    // Метод для удаления данных из провайдера контента
     @Override
     public int delete(Uri uri, String selection, String[] selectionArgs) {
         int count = 0;
@@ -113,30 +118,29 @@ public class MyContentProvider extends ContentProvider {
         getContext().getContentResolver().notifyChange(uri, null);
         return count;
     }
-
     private SQLiteDatabase db;
-
+    // Название базы данных
     static final String DATABASE_NAME = "UserDB";
-
+    // Название таблицы
     static final String TABLE_NAME = "Users";
-
+    // Версия базы данных
     static final int DATABASE_VERSION = 1;
-
+    // SQL-запрос для создания таблицы
     static final String CREATE_DB_TABLE = " CREATE TABLE " + TABLE_NAME
             + " (id INTEGER PRIMARY KEY AUTOINCREMENT, "
             + " name TEXT NOT NULL);";
 
+    // Вспомогательный класс для работы с базой данных
     private static class DatabaseHelper extends SQLiteOpenHelper {
         DatabaseHelper(Context context) {
             super(context, DATABASE_NAME, null, DATABASE_VERSION);
         }
-
+        // Метод, вызываемый при создании базы данных
         @Override
         public void onCreate(SQLiteDatabase db) {
-
             db.execSQL(CREATE_DB_TABLE);
         }
-
+        // Метод, вызываемый при обновлении базы данных
         @Override
         public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
             db.execSQL("DROP TABLE IF EXISTS " + TABLE_NAME);
--- app/src/main/java/com/example/proekt_12/User.java
+++ app/src/main/java/com/example/proekt_12/User.java
@@ -1,15 +1,7 @@
 package com.example.proekt_12;
 
 public class User {
-    String name, email;
-    int age;
-
-    public User(String name, String email, int age) {
-        this.name = name;
-        this.email = email;
-        this.age = age;
-    }
-
-    public User() {
-    }
+    public String name;
+    public int age;
+    public String email;
 }
